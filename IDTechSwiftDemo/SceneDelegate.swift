4f31e02e9aeb1f41bf7da3f09f75e3c4b23536eb
David Higginbotham <dhigginbotham@clearent.com>
2019-10-24 09:39:56 -0500

    this works

--- IDTechSwiftDemo/SceneDelegate.swift
+++ IDTechSwiftDemo/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        let baseUrl = "https/gateway-sb.clearent.net"
+        let baseUrl = "https://gateway-sb.clearent.net"
         let publicKey = "307a301406072a8648ce3d020106092b240303020801010c036200042b0cfb3a1faaca8fb779081717a0bafb03e0cb061a1ef297f75dc5b951aaf163b0c2021e9bb73071bf89c711070e96ab1b63c674be13041d9eb68a456eb6ae63a97a9345c120cd8bff1d5998b2ebbafc198c5c5b26c687bfbeb68b312feb43bf"
         let clearentPaymentView = ClearentPaymentView(baseUrl, publicKey)
         
